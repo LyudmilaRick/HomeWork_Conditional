0bff00719289bf3964df793383e157bf96d0d068
RickLyudmila <Lrick@yandex.ru>
2021-10-13 00:55:27 +0300

    Lesson_Conditional

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        // 12.10.2021
         // Задание 1 iOS VS Android
         // использую BYTE ибо точно знаю, что значение 0 или 1
         byte clientOS = 0;  // iOS is better
@@ -38,7 +39,7 @@ public class Main {
         }
         // Задание 3
         System.out.println("// Задание 3");
-        int year = 3033;
+        int year = 3020;
         int valueMod; //Оператор деления по модулю
         boolean isLeapYear = false;
         // если ни один if  не отработает - значение isLeapYear не изменится!
@@ -54,8 +55,120 @@ public class Main {
         } else {
             System.out.println(year + " год не  является високосным");
         }
+        // Задание 4
+        System.out.println("// Задание 4");
+        int deliveryDistance = 95;
+        int deliveryInitial = 1;
+        int deliveryStep = 1;
+        int deliveryTime;
+        if (deliveryDistance <= 20) {
+            deliveryTime = deliveryInitial;
+        } else if (deliveryDistance <= 60) {
+            deliveryTime = deliveryInitial + deliveryStep;
+        } else if (deliveryDistance <= 100) {
+            deliveryTime = deliveryInitial + deliveryStep * 2;
+        } else {
+            deliveryTime = deliveryInitial + deliveryStep * 3;
+        }
+        System.out.println("Потребуется дней: " + deliveryTime);
+        // Задание 5
+        System.out.println("// Задание 5");
+        int monthNumber = 10;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Январь - зима");
+                break;
+            case 2:
+                System.out.println("Февраль - зима");
+                break;
+            case 3:
+                System.out.println("Март - весна");
+                break;
+            case 4:
+                System.out.println("Апрель - весна");
+                break;
+            case 5:
+                System.out.println("Май -  весна");
+                break;
+            case 6:
+                System.out.println("Июнь - лето");
+                break;
+            case 7:
+                System.out.println("Июль - лето");
+                break;
+            case 8:
+                System.out.println("Август - лето");
+                break;
+            case 9:
+                System.out.println("Сентябрь -осень");
+                break;
+            case 10:
+                System.out.println("Октябрь -осень");
+                break;
+            case 11:
+                System.out.println("Ноябрь -осень");
+                break;
+            case 12:
+                System.out.println("Декабрь - зима");
+                break;
+            default:
+                System.out.println("Период указан некорректно");
+        }
+        // Задание 6. Повышенная сложность.
+        System.out.println("// Задание 6");
+        int age = 19;
+        int salary = 58_000;
+        float koefCalc;
+        // check age
+        if (age < 23) {
+            koefCalc = 2f;
+        } else {
+            koefCalc = 3f;
+        }
+        // check salary
+        if (salary < 50_000) {
+            // koefCalc is the same
+        }
+        else if (salary <= 80_000) {
+            koefCalc = koefCalc * 1.2f;
+        } else  {
+            koefCalc = koefCalc * 1.5f;
+        }
+        float limitClient = salary * koefCalc;
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + limitClient + " рублей");
 
+        // Задание 7. Повышенная сложность.
+        System.out.println("// Задание 7");
+        int ageClient = 25;
+        int salaryClient = 60_000;
+        float wantedSum = 330_000f;
+        float maxPayment = salaryClient * 50.00f / 100.00f;
 
+        float rateBase = 10f;
+        float calcRate;
+        // first check
+        if (ageClient < 23 ) {
+            rateBase = rateBase + 1f;
+        }
+        else if (ageClient < 30 ) {
+            rateBase = rateBase + 0.5f;
+        }
+        // second  check
+        if (salaryClient  > 80_000) {
+            rateBase = rateBase - 0.7f;
+        }
+
+        float wantedSumCalc =  wantedSum / 100 * (100 + rateBase);
+        float monthPayment  =  wantedSumCalc / 12;
+        System.out.println("Максимальный платеж при ЗП " + salaryClient + " равен " + maxPayment + " рублей");
+        System.out.println("Платеж по кредиту равен " + monthPayment);
+        if (maxPayment > monthPayment ) {
+            System.out.println("Одобрено" );
+        } else {
+            System.out.println("Отказано" );
+        }
+        }
     }
-}
+
+
 
